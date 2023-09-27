9ddcb1f00b2957689ced4d829f00454776b0494b
beicause <1494181792@qq.com>
2023-09-27 10:15:49 +0800

    fix theme

--- src/theme.ts
+++ src/theme.ts
@@ -6,8 +6,6 @@ import { App } from "@capacitor/app";
 export const themeMode = ref("system");
 const M = registerPlugin("M") as any
 
-Preferences.get({ key: 'themeMode' }).then(res => themeMode.value = res.value || "system")
-
 const prefersDark = async () => M.getThemeMode ? (await M.getThemeMode()).isDark : window.matchMedia('(prefers-color-scheme: dark)').matches;
 function toggleDarkTheme(shouldAdd: boolean) {
     document.body.classList.toggle('dark', shouldAdd);
@@ -26,8 +24,10 @@ watch(themeMode, async (value: string, _) => {
             toggleDarkTheme(false);
             break;
     }
-}, { immediate: true })
+})
 
 App.addListener("resume", async () => {
     themeMode.value === "system" && toggleDarkTheme(await prefersDark());
 })
+
+Preferences.get({ key: 'themeMode' }).then(res => themeMode.value = res.value || "system")
