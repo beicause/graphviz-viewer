4c9d7cc68cb0ecca4e556f2b2048d0653bd363b6
beicause <1494181792@qq.com>
2023-10-22 19:44:27 +0800

    rename gv to graphvize, add mobads

---
+++ android/app/src/main/java/indie/haozi/gvviewer/App.kt
@@ -0,0 +1,200 @@
+package indie.haozi.gvviewer
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.appopen.AppOpenAd
+import java.util.Date
+
+private const val LOG_TAG = "AppOpenAdManager"
+private const val AD_UNIT_ID = "ca-app-pub-9433844779206817/8811733125"
+
+class App : Application(), Application.ActivityLifecycleCallbacks, LifecycleEventObserver {
+    private lateinit var appOpenAdManager: AppOpenAdManager
+    private var currentActivity: Activity? = null
+
+    /** Interface definition for a callback to be invoked when an app open ad is complete. */
+    interface OnShowAdCompleteListener {
+        fun onShowAdComplete()
+    }
+
+    /** Inner class that loads and shows app open ads. */
+    private inner class AppOpenAdManager {
+        private var appOpenAd: AppOpenAd? = null
+        private var isLoadingAd = false
+        var isShowingAd = false
+        private var loadTime: Long = 0
+
+        /** Check if ad exists and can be shown. */
+        private fun isAdAvailable(): Boolean {
+            return appOpenAd != null && wasLoadTimeLessThanNHoursAgo(4)
+        }
+
+
+        /** Request an ad. */
+        fun loadAd(context: Context) {
+            // Do not load ad if there is an unused ad or one is already loading.
+            if (isLoadingAd || isAdAvailable()) {
+                return
+            }
+
+            isLoadingAd = true
+            val request = AdRequest.Builder().build()
+            AppOpenAd.load(
+                context, AD_UNIT_ID, request,
+//                AppOpenAd.APP_OPEN_AD_ORIENTATION_PORTRAIT,
+                object : AppOpenAd.AppOpenAdLoadCallback() {
+
+                    override fun onAdLoaded(ad: AppOpenAd) {
+                        // Called when an app open ad has loaded.
+                        Log.d(LOG_TAG, "Ad was loaded.")
+                        appOpenAd = ad
+                        isLoadingAd = false
+                        loadTime = Date().time
+                    }
+
+                    override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                        // Called when an app open ad has failed to load.
+                        Log.d(LOG_TAG, loadAdError.message)
+                        isLoadingAd = false;
+                    }
+                })
+        }
+
+        private fun wasLoadTimeLessThanNHoursAgo(numHours: Long): Boolean {
+            val dateDifference: Long = Date().time - loadTime
+            val numMilliSecondsPerHour: Long = 3600000
+            return dateDifference < numMilliSecondsPerHour * numHours
+        }
+
+        /** Shows the ad if one isn't already showing. */
+        fun showAdIfAvailable(
+            activity: Activity,
+            onShowAdCompleteListener: OnShowAdCompleteListener
+        ) {
+            // If the app open ad is already showing, do not show the ad again.
+            if (isShowingAd) {
+                Log.d(LOG_TAG, "The app open ad is already showing.")
+                return
+            }
+
+            // If the app open ad is not available yet, invoke the callback then load the ad.
+            if (!isAdAvailable()) {
+                Log.d(LOG_TAG, "The app open ad is not ready yet.")
+                onShowAdCompleteListener.onShowAdComplete()
+                loadAd(activity)
+                return
+            }
+
+            appOpenAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+
+                override fun onAdDismissedFullScreenContent() {
+                    // Called when full screen content is dismissed.
+                    // Set the reference to null so isAdAvailable() returns false.
+                    Log.d(LOG_TAG, "Ad dismissed fullscreen content.")
+                    appOpenAd = null
+                    isShowingAd = false
+
+                    onShowAdCompleteListener.onShowAdComplete()
+                    loadAd(activity)
+                }
+
+                override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                    // Called when fullscreen content failed to show.
+                    // Set the reference to null so isAdAvailable() returns false.
+                    Log.d(LOG_TAG, adError.message)
+                    appOpenAd = null
+                    isShowingAd = false
+
+                    onShowAdCompleteListener.onShowAdComplete()
+                    loadAd(activity)
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                    // Called when fullscreen content is shown.
+                    Log.d(LOG_TAG, "Ad showed fullscreen content.")
+                }
+            }
+            isShowingAd = true
+            appOpenAd?.show(activity)
+        }
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        registerActivityLifecycleCallbacks(this)
+        MobileAds.initialize(this) {}
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+        appOpenAdManager = AppOpenAdManager()
+
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+
+    override fun onActivityStarted(activity: Activity) {
+        // Updating the currentActivity only when an ad is not showing.
+        if (!appOpenAdManager.isShowingAd) {
+            currentActivity = activity
+        }
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+//        TODO("Not yet implemented")
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+//        TODO("Not yet implemented")
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+//        TODO("Not yet implemented")
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+//        TODO("Not yet implemented")
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+//        TODO("Not yet implemented")
+    }
+
+
+    /** LifecycleObserver method that shows the app open ad when the app moves to foreground. */
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        when (event) {
+            Lifecycle.Event.ON_START -> {
+                currentActivity?.let {
+                    showAdIfAvailable(it)
+                }
+            }
+
+            else -> {}
+        }
+    }
+
+    /** Show the ad if one isn't already showing. */
+    private fun showAdIfAvailable(activity: Activity) {
+        appOpenAdManager.showAdIfAvailable(
+            activity,
+            object : OnShowAdCompleteListener {
+                override fun onShowAdComplete() {
+                    // Empty because the user will go back to the activity that shows the ad.
+                }
+            })
+    }
+
+}
--- android/app/src/main/java/indie/haozi/gvviewer/MainActivity.kt
+++ android/app/src/main/java/indie/haozi/gvviewer/MainActivity.kt
@@ -5,16 +5,74 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import com.getcapacitor.BridgeActivity
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.OnUserEarnedRewardListener
+import com.google.android.gms.ads.rewarded.RewardItem
+import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAd
+import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAdLoadCallback
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
 
 var intentViewStr: String? = null
+private const val AD_UNIT_ID = "ca-app-pub-9433844779206817/3683529521"
 
-class MainActivity : BridgeActivity() {
+class MainActivity : BridgeActivity(), OnUserEarnedRewardListener {
+    private final var TAG = "MainActivity"
+    private var rewardedInterstitialAd:RewardedInterstitialAd? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         registerPlugin(MPlugin::class.java)
         super.onCreate(savedInstanceState)
+    }
+
+    private fun loadAd() {
+        RewardedInterstitialAd.load(this, AD_UNIT_ID,
+                    AdRequest.Builder().build(), object : RewardedInterstitialAdLoadCallback() {
+                override fun onAdLoaded(ad: RewardedInterstitialAd) {
+                    Log.d(TAG, "Ad was loaded.")
+                    rewardedInterstitialAd = ad
+                    rewardedInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+                        override fun onAdClicked() {
+                            // Called when a click is recorded for an ad.
+                            Log.d(TAG, "Ad was clicked.")
+                        }
+
+                        override fun onAdDismissedFullScreenContent() {
+                            // Called when ad is dismissed.
+                            // Set the ad reference to null so you don't show the ad a second time.
+                            Log.d(TAG, "Ad dismissed fullscreen content.")
+                            rewardedInterstitialAd = null
+                        }
+
+                        override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                            // Called when ad fails to show.
+                            Log.e(TAG, "Ad failed to show fullscreen content.")
+                            rewardedInterstitialAd = null
+                        }
+
+                        override fun onAdImpression() {
+                            // Called when an impression is recorded for an ad.
+                            Log.d(TAG, "Ad recorded an impression.")
+                        }
+
+                        override fun onAdShowedFullScreenContent() {
+                            // Called when ad is shown.
+                            Log.d(TAG, "Ad showed fullscreen content.")
+                        }
+                    }
+
+                }
+
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    Log.d(TAG, adError.toString())
+                    rewardedInterstitialAd = null
+                }
+            })
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -36,5 +94,10 @@ class MainActivity : BridgeActivity() {
                 Log.d("Receive intent view", str)
             }
         }
+    }
+
+    override fun onUserEarnedReward(p0: RewardItem) {
+        Log.d(TAG, "User earned reward.")
+        TODO("Not yet implemented")
     }
 }
